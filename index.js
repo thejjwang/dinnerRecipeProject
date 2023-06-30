e45fbaacd8bda85d3138221987de044ddbc78353
thejjwang <jonathanjacobwang@gmail.com>
2023-06-30 14:03:48 -0700

    moved column so it looks better

---
+++ index.js
@@ -0,0 +1,12 @@
+let addRecipeBtn = document.getElementById('addRecipeBtn');
+addRecipeBtn.addEventListener('click', addRecipe);
+function addRecipe(){
+
+}
+
+let letsCookBtn = document.getElementById('letsCookBtn');
+letsCookBtn.addEventListener('click', letsCook);
+function letsCook() {
+
+}
+let sideInput = document.getElementById('side');
