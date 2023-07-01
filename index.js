1b2706476fcd625d4723c58d546aeb2786616b9e
thejjwang <jonathanjacobwang@gmail.com>
2023-06-30 21:31:57 -0700

    updated a bit still has some edge cases

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ function addRecipe() {
     const submitButton = document.createElement('button');
     submitButton.setAttribute('id', 'addYourOwnRecipeBtn');
     submitButton.innerText = "Submit";
+    // go back button inside the div
+    const backButton = document.createElement('button');
+    backButton.setAttribute('id', 'backBtn');
+    backButton.innerText = "Go Back";
     // add br in between elements in the div
     const br1 = document.createElement('br');
     const br2 = document.createElement('br');
@@ -47,7 +51,7 @@ function addRecipe() {
     const br8 = document.createElement('br');
 
     
-    // appending the h2, inputs, and button to recipeDiv
+    // appending the h2, inputs, and buttons to recipeDiv
     recipeDiv.appendChild(recipeTitle);
     recipeDiv.appendChild(br1);
     // recipeDiv.appendChild(br2);
@@ -61,6 +65,8 @@ function addRecipe() {
     recipeDiv.appendChild(br7);
     // recipeDiv.appendChild(br8);
     recipeDiv.appendChild(submitButton);
+    recipeDiv.appendChild(br8);
+    recipeDiv.appendChild(backButton);
     
     column1.appendChild(recipeDiv);
     // need to add the styling for the new created div
@@ -72,11 +78,59 @@ function addRecipe() {
     submitButton.style.display = 'block';
     submitButton.style.backgroundColor = 'teal';
     submitButton.style.width = '150px';
+    backButton.style.backgroundColor = 'teal';
+    backButton.style.width = '150px';
+    
+    // back button
+    backButton.addEventListener('click', goBack);
+    function goBack() {
+        window.location.reload();
+    }
 
+    // submit button
+    submitButton.addEventListener('click', clearColumn1Again);
+    function clearColumn1Again(){
+        // column1.innerHTML = '';
+        submitOwnRecipe();
+    }
+    function submitOwnRecipe(){
+    // clears img by removing child element of the parent node
+    img.parentNode.removeChild(img);
+    if (sideInput.value === "" && mainInput.value === ""){
+        meal.innerText = dessertInput.value;
+        dessertArr.push(dessertInput.value);
+    } else if (sideInput.value === "" && dessertInput.value === ""){
+        meal.innerText = mainInput.value;
+        mainArr.push(mainInput.value);
+    } else if (dessertInput.value === "" && mainInput.value === ""){
+        meal.innerText = sideInput.value;
+        sideArr.push(sideInput.value);
+    } else if (sideInput.value === ""){
+        meal.innerText = `Your main is a ${mainInput.value} and for dessert a ${dessertInput.value}`;
+        mainArr.push(mainInput.value);
+        dessertArr.push(dessertInput.value);
+    } else if (mainInput.value === ""){
+        meal.innerText = `Your side is a ${sideInput.value} and for dessert a ${dessertInput.value}`;
+        sideArr.push(sideInput.value);
+        dessertArr.push(dessertInput.value);
+    } else if (dessertInput.value === ""){
+        meal.innerText = `Your side is a ${sideInput.value} and the main is ${mainInput.value}`;
+        mainArr.push(mainInput.value);
+        dessertArr.push(dessertInput.value);
+    } else {
+        meal.innerText = `The meal consists of a side of ${sideInput.value}, and with the main dish being ${mainInput.value}, and finally for dessert a ${dessertInput.value}.`;
+        mainArr.push(mainInput.value);
+        dessertArr.push(dessertInput.value);
+        sideArr.push(sideInput.value);
+    } 
+    } 
+    clearColumn2();
 }
-
+function clearColumn2() {
+    meal.innerHTML = '';
+}
 // input for user then when click addrecipe button it adds their recipe to the corresponding arr
-// and immediately show on the cooking pot
+
 letsCookBtn.addEventListener('click', letsCook);
 function letsCook() {
     // loop through button of our buttons
@@ -107,3 +161,4 @@ function letsCook() {
     
     
 }
+
