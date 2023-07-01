02140bf6abfc767f3ddf1973184e3437ae12437c
thejjwang <jonathanjacobwang@gmail.com>
2023-06-30 20:40:57 -0700

    styled new column and refactored js for it

--- index.js
+++ index.js
@@ -7,23 +7,82 @@ let dessertArr = ['cookie', 'cake', 'brownie', 'ice cream'];
 let entireArr = [];
 let img = document.getElementById('img');
 let meal = document.getElementById('meal');
-let column = document.getElementById('column');
+let column1 = document.getElementById('column1');
 
-addRecipeBtn.addEventListener('click', addRecipe);
-function addRecipe(){
+addRecipeBtn.addEventListener('click', clearColumn1);
+function clearColumn1(){
+    column1.innerHTML = "";
+    addRecipe();
+}
+function addRecipe() {
+    // create new div
+    const recipeDiv = document.createElement('div');
+    // h2 inside the div
+    const recipeTitle = document.createElement('h2');
+    recipeTitle.innerText = 'Add Your Own Recipe';
+    // side input inside the div
+    const sideInput = document.createElement('input');
+    sideInput.setAttribute('type', 'text');
+    sideInput.setAttribute('placeholder', 'Add Your Side');
+    // main input inside the div
+    const mainInput = document.createElement('input');
+    mainInput.setAttribute('type', 'text');
+    mainInput.setAttribute('placeholder', 'Add Your Main');
+    // dessert input inside the div
+    const dessertInput = document.createElement('input');
+    dessertInput.setAttribute('type', 'text');
+    dessertInput.setAttribute('placeholder', 'Add Your Dessert');
+    // submit button inside the div
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('id', 'addYourOwnRecipeBtn');
+    submitButton.innerText = "Submit";
+    // add br in between elements in the div
+    const br1 = document.createElement('br');
+    const br2 = document.createElement('br');
+    const br3 = document.createElement('br');
+    const br4 = document.createElement('br');
+    const br5 = document.createElement('br');
+    const br6 = document.createElement('br');
+    const br7 = document.createElement('br');
+    const br8 = document.createElement('br');
+
+    
+    // appending the h2, inputs, and button to recipeDiv
+    recipeDiv.appendChild(recipeTitle);
+    recipeDiv.appendChild(br1);
+    // recipeDiv.appendChild(br2);
+    recipeDiv.appendChild(sideInput);
+    recipeDiv.appendChild(br3);
+    // recipeDiv.appendChild(br4);
+    recipeDiv.appendChild(mainInput);
+    recipeDiv.appendChild(br5);
+    // recipeDiv.appendChild(br6);
+    recipeDiv.appendChild(dessertInput);
+    recipeDiv.appendChild(br7);
+    // recipeDiv.appendChild(br8);
+    recipeDiv.appendChild(submitButton);
+    
+    column1.appendChild(recipeDiv);
+    // need to add the styling for the new created div
+    recipeDiv.style.marginTop = '50px';
+    recipeDiv.style.marginLeft = '40px';
+    sideInput.style.display = 'block';
+    mainInput.style.display = 'block';
+    dessertInput.style.display = 'block';
+    submitButton.style.display = 'block';
+    submitButton.style.backgroundColor = 'teal';
+    submitButton.style.width = '150px';
 
 }
+
 // input for user then when click addrecipe button it adds their recipe to the corresponding arr
 // and immediately show on the cooking pot
 letsCookBtn.addEventListener('click', letsCook);
 function letsCook() {
-    let selectedDish;
     // loop through button of our buttons
     for (const radioButton of radioButtons) {
         // whatever button is selected(checked)
         if (radioButton.checked) {
-            selectedDish = radioButton.value;
-            console.log(radioButton.value);
             if (radioButton.value === 'side'){
                 // setting text to a random side value
                 meal.innerText = sideArr[Math.floor(Math.random() * sideArr.length)];
